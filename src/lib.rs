2da3463c66b14fec74889926bee0858a6c6d8488
Nick Wilson <logikfx@gmail.com>
2018-03-03 23:49:40 -0500

    broke program up into seperate files to make more modular

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,35 @@
 /// lib.rs
 use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
 
-fn handle_file(&filename) {
-    let mut f = File::open(filename).expect("file not found");
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+// constructor for Config
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
-        f.read_to_string(&mut contents)
-            .expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)?;
 
-        println!("With text:\n{}", contents);
+    println!("With text:\n{}", contents);
 
+    Ok(())
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,25 @@
 /// main.rs
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(config.filename).expect("file not found");
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-// constructor for Config
-impl Config {
-    fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            panic!("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Config { query, filename }
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
+
